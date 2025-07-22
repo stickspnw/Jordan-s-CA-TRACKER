1eea91abc7f001dec76d600cde355b3d6dd8638d
stickspnw <stickspnw@gmail.com>
2025-07-22 16:24:08 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7989890463:AAGLPsMpVIE8Vd-xvszcpv_fADtXcmZg8ps"  # Your actual bot token
+CHAT_ID = 6735416584  # Your Telegram ID (used for alerts)
+FEE_WALLET = "Fg692e1bX79276mq2fSwQeoRJoude4KnVJSWEXXJ6Rwa"  # Bot's internal fee wallet
+
+# For Solana network
+SOLANA_RPC = "https://api.mainnet-beta.solana.com"  # You can change to devnet if testing
